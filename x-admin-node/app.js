106e3a5a21ce2a0412fc53571b7d9000205ab117
xmw <571692056@qq.com>
2022-02-04 18:04:09 +0800

    feat: view url change

--- x-admin-node/app.js
+++ x-admin-node/app.js
@@ -28,7 +28,9 @@ app.use(bodyparser({
 }))
 app.use(json())
 // app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(require('koa-static')(__dirname + '/public', {
+  gizp: true
+}))
 
 app.use(views(__dirname + '/views', {
   extension: 'pug'
