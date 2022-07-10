aa4d506a11420d4f1eb4a0f714a7b785ebc3660f
xmw <571692056@qq.com>
2022-07-10 20:34:37 +0800

    feat: ignore download

--- src/router/index.ts
+++ src/router/index.ts
@@ -68,7 +68,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   NProgress.start()
-  if (to.path === '/login') {
+  if (to.path === '/login' || to.path === '/download') {
     next()
     return
   }
